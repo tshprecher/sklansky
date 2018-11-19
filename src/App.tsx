c6ab01bf27ebf47240ee6ae1d0247474f42b86b3
Tal Shprecher <tshprecher@gmail.com>
2018-11-18 22:49:46 -0800

    rework the sklansky table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.css';
 
+// Model
+
 enum Suit {
     Clubs = 1,
     Diamonds,
@@ -9,7 +11,7 @@ enum Suit {
 }
 
 enum Value {
-    One, Two, Three, Four, Five, Six, Seven,
+    Two = 2, Three, Four, Five, Six, Seven,
     Eight, Nine, Ten, Jack, Queen, King, Ace
 }
 
@@ -20,8 +22,31 @@ class Card {
         this.value = v;
         this.suit = s;
     }
-    toString() {
-        return "TODO: implement"
+    toString(): string {
+        const strVal = new Map<number, string>([
+            [Value.Two, "2"],
+            [Value.Three, "3"],
+            [Value.Four, "4"],
+            [Value.Five, "5"],
+            [Value.Six, "6"],
+            [Value.Seven, "7"],
+            [Value.Eight, "8"],
+            [Value.Nine, "9"],
+            [Value.Ten, "10"],
+            [Value.Jack, "J"],
+            [Value.Queen, "Q"],
+            [Value.King, "K"],
+            [Value.Ace, "A"],
+        ]);
+
+        const strSuit = new Map<number, string>([
+            [Suit.Clubs, "C"],
+            [Suit.Diamonds, "D"],
+            [Suit.Hearts, "H"],
+            [Suit.Spades, "S"],
+        ]);
+
+        return strVal[this.value] + strSuit[this.suit];
     }
 }
 
@@ -34,12 +59,32 @@ class StartingHand {
         this.value2 = v2;
         this.suited = suited;
     }
+
+    equals(sh: StartingHand): boolean {
+        if (this.suited !== sh.suited) {
+            return false;
+        }
+
+        let thisLower = this.value1
+        let thisUpper = this.value2
+        let shLower = sh.value1
+        let shUpper = sh.value2
+
+        if (thisLower > thisUpper) {
+            [thisLower, thisUpper] = [thisUpper, thisLower]
+        }
+        if (shLower > shUpper) {
+            [shLower, shUpper] = [shUpper, shLower]
+        }
+
+        return thisLower === shLower && thisUpper === shUpper;
+    }
 }
 
-// encode the Sklansky table (http://pokermistery.com/starting-hands-chart)
+// encode the Sklansky table (https://en.wikipedia.org/wiki/Texas_hold_%27em_starting_hands)
 const SklanskyTable = {
     1: [
-        new StartingHand(Value.Ace, Value.King, false),
+        new StartingHand(Value.Ace, Value.Ace, false),
         new StartingHand(Value.King, Value.King, false),
         new StartingHand(Value.Queen, Value.Queen, false),
         new StartingHand(Value.Jack, Value.Jack, false),
@@ -53,70 +98,65 @@ const SklanskyTable = {
     ],
     3: [
         new StartingHand(Value.Nine, Value.Nine, false),
-        new StartingHand(Value.Jack, Value.Ten, true),
-        new StartingHand(Value.Queen, Value.Jack, true),
-        new StartingHand(Value.King, Value.Jack, true),
         new StartingHand(Value.Ace, Value.Ten, true),
+        new StartingHand(Value.King, Value.Jack, true),
+        new StartingHand(Value.Queen, Value.Jack, true),
+        new StartingHand(Value.Jack, Value.Ten, true),
         new StartingHand(Value.Ace, Value.Queen, false),
     ],
     4: [
-        new StartingHand(Value.Ten, Value.Nine, true),
-        new StartingHand(Value.King, Value.Queen, false),
         new StartingHand(Value.Eight, Value.Eight, false),
-        new StartingHand(Value.Queen, Value.Ten, true),
-        new StartingHand(Value.Nine, Value.Eight, true),
-        new StartingHand(Value.Jack, Value.Nine, true),
-        new StartingHand(Value.Ace, Value.Jack, false),
         new StartingHand(Value.King, Value.Ten, true),
+        new StartingHand(Value.Queen, Value.Ten, true),
+        new StartingHand(Value.Jack, Value.Nine, true),
+        new StartingHand(Value.Ten, Value.Nine, true),
+        new StartingHand(Value.Nine, Value.Eight, true),
+        new StartingHand(Value.Ace, Value.Jack, false),
+        new StartingHand(Value.King, Value.Queen, false),
     ],
     5: [
         new StartingHand(Value.Seven, Value.Seven, false),
-        new StartingHand(Value.Eight, Value.Seven, true),
+
+        // fill in the rest of the suited aces
+        new StartingHand(Value.Ace, Value.Nine, true),
+        new StartingHand(Value.Ace, Value.Eight, true),
+        new StartingHand(Value.Ace, Value.Seven, true),
+        new StartingHand(Value.Ace, Value.Six, true),
+        new StartingHand(Value.Ace, Value.Five, true),
+        new StartingHand(Value.Ace, Value.Four, true),
+        new StartingHand(Value.Ace, Value.Three, true),
+        new StartingHand(Value.Ace, Value.Two, true),
+
         new StartingHand(Value.Queen, Value.Nine, true),
-        new StartingHand(Value.Ten, Value.Eight, true),                        
+        new StartingHand(Value.Ten, Value.Eight, true),
+        new StartingHand(Value.Nine, Value.Seven, true),
+        new StartingHand(Value.Eight, Value.Seven, true),
+        new StartingHand(Value.Seven, Value.Six, true),
         new StartingHand(Value.King, Value.Jack, false),
         new StartingHand(Value.Queen, Value.Jack, false),
         new StartingHand(Value.Jack, Value.Ten, false),
-        new StartingHand(Value.Seven, Value.Six, true),                                                
-        new StartingHand(Value.Nine, Value.Seven, true),                                                
 
-        // fill in the rest of the suited aces
-        new StartingHand(Value.Ace, Value.Nine, true),                                                
-        new StartingHand(Value.Ace, Value.Eight, true),                                                
-        new StartingHand(Value.Ace, Value.Seven, true),                                                
-        new StartingHand(Value.Ace, Value.Six, true),                                                
-        new StartingHand(Value.Ace, Value.Five, true),                                                                        
-        new StartingHand(Value.Ace, Value.Four, true),                                                                        
-        new StartingHand(Value.Ace, Value.Three, true),                                                                        
-        new StartingHand(Value.Ace, Value.Two, true),                                                                        
- 
-        new StartingHand(Value.Six, Value.Five, true),                                                
+
+        new StartingHand(Value.Six, Value.Five, true), // TODO: double check this. where does this belong?
     ],
     6: [
         new StartingHand(Value.Six, Value.Six, false),
-        new StartingHand(Value.Ace, Value.Ten, false),
         new StartingHand(Value.Five, Value.Five, true),
-        new StartingHand(Value.Eight, Value.Six, true),
-        new StartingHand(Value.King, Value.Ten, false),
-        new StartingHand(Value.Queen, Value.Ten, false),
-        new StartingHand(Value.Five, Value.Four, true),
         new StartingHand(Value.King, Value.Nine, true),
         new StartingHand(Value.Jack, Value.Eight, true),
+        new StartingHand(Value.Eight, Value.Six, true),
         new StartingHand(Value.Seven, Value.Five, true),
+        new StartingHand(Value.Five, Value.Four, true),
+        new StartingHand(Value.Ace, Value.Ten, false),
+        new StartingHand(Value.King, Value.Ten, false),
+        new StartingHand(Value.Queen, Value.Ten, false),
     ],
     7: [
         new StartingHand(Value.Four, Value.Four, false),
-        new StartingHand(Value.Jack, Value.Nine, false),
-        new StartingHand(Value.Six, Value.Four, true),
-        new StartingHand(Value.Ten, Value.Nine, false),
-        new StartingHand(Value.Five, Value.Three, true),
         new StartingHand(Value.Three, Value.Three, false),
-        new StartingHand(Value.Nine, Value.Eight, false),
-        new StartingHand(Value.Four, Value.Three, true),
         new StartingHand(Value.Two, Value.Two, false),
-        
+
         // fill in the rest of the suited kings
-        new StartingHand(Value.King, Value.Nine, true),
         new StartingHand(Value.King, Value.Eight, true),
         new StartingHand(Value.King, Value.Seven, true),
         new StartingHand(Value.King, Value.Six, true),
@@ -125,33 +165,59 @@ const SklanskyTable = {
         new StartingHand(Value.King, Value.Three, true),
         new StartingHand(Value.King, Value.Two, true),
 
-        new StartingHand(Value.Ten, Value.Seven, true),
         new StartingHand(Value.Queen, Value.Eight, true),
+        new StartingHand(Value.Ten, Value.Seven, true),
+        new StartingHand(Value.Six, Value.Four, true),
+        new StartingHand(Value.Five, Value.Three, true),
+        new StartingHand(Value.Four, Value.Three, true),
+        new StartingHand(Value.Jack, Value.Nine, false),
+        new StartingHand(Value.Ten, Value.Nine, false),
+        new StartingHand(Value.Nine, Value.Eight, false),
     ],
     8: [
-        new StartingHand(Value.Eight, Value.Seven, false),
-        new StartingHand(Value.Ace, Value.Nine, false),
-        new StartingHand(Value.Queen, Value.Nine, false),
-        new StartingHand(Value.Seven, Value.Six, false),
-        new StartingHand(Value.Four, Value.Two, true),
-        new StartingHand(Value.Three, Value.Two, true),
+        new StartingHand(Value.Jack, Value.Seven, true),
         new StartingHand(Value.Nine, Value.Six, true),
         new StartingHand(Value.Eight, Value.Five, true),
-        new StartingHand(Value.Five, Value.Eight, false),
-        new StartingHand(Value.Jack, Value.Seven, true),
+        new StartingHand(Value.Seven, Value.Four, true),
+        new StartingHand(Value.Four, Value.Two, true),
+        new StartingHand(Value.Three, Value.Two, true),
+        new StartingHand(Value.Ace, Value.Nine, false),
+        new StartingHand(Value.King, Value.Nine, false),
+        new StartingHand(Value.Queen, Value.Nine, false),
+        new StartingHand(Value.Jack, Value.Eight, false),
+        new StartingHand(Value.Ten, Value.Eight, false),
+        new StartingHand(Value.Eight, Value.Seven, false),
+        new StartingHand(Value.Seven, Value.Six, false),
         new StartingHand(Value.Six, Value.Five, false),
         new StartingHand(Value.Five, Value.Four, false),
-        new StartingHand(Value.Seven, Value.Four, true),
-        new StartingHand(Value.King, Value.Nine, false),
-        new StartingHand(Value.Ten, Value.Eight, false),
     ]
 }
 
+// getSklanskyValue takes a starting hand and returns the Sklansky rank
+// from 1 to 8, or -1 if it is unplayable.
 function getSklanskyValue(hand: StartingHand): number {
+    let rank: number;
+    for (rank = 1; rank < 9; rank++) {
+        let hands = SklanskyTable[rank]
+        let h: number;
+        for (h = 0; h < hands.length; h++) {
+            if (hand.equals(hands[h])) {
+                return rank;
+            }
+        }
+    }
     return -1;
 }
 
+// Views
 
+/*function SklanskyCell({ startingHand: StartingHand }) {
+    return (
+        <div className="hello">
+            TODO: fill in sklansky cell
+      </div>
+    );
+}*/
 
 class App extends React.Component {
     public render() {
@@ -161,7 +227,10 @@ class App extends React.Component {
                     <h1 className="App-title">Learn Your Sklansky Table</h1>
                 </header>
                 <div>
-                    TODO: fill in cards + {new Card(Value.One, Suit.Clubs).toString()}
+                    TODO: fill in cards + {new Card(Value.Ace, Suit.Clubs).toString()}
+                </div>
+                <div>
+                    Test: Slansky rank of AA: + {getSklanskyValue(new StartingHand(Value.Ace, Value.King, false))}
                 </div>
                 <div>
 
