548c581cdb75cc38649843f56ce13bbca1db94f4
Tal Shprecher <tshprecher@gmail.com>
2018-12-08 16:33:21 -0800

    Updates

---
+++ precache-manifest.d7a578f97eb66da47c10c348dae67d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d52b95c221fc50eba5ff",
+    "url": "/sklansky/static/js/runtime~main.d52b95c2.js"
+  },
+  {
+    "revision": "c223be7be0a3861e8d76",
+    "url": "/sklansky/static/js/main.c223be7b.chunk.js"
+  },
+  {
+    "revision": "d34cca0a8a6da93016c7",
+    "url": "/sklansky/static/js/1.d34cca0a.chunk.js"
+  },
+  {
+    "revision": "c223be7be0a3861e8d76",
+    "url": "/sklansky/static/css/main.a69e6db3.chunk.css"
+  },
+  {
+    "revision": "9e5333c339622c37322daa56ec6c6073",
+    "url": "/sklansky/index.html"
+  }
+];
